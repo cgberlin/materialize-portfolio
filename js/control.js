f0f7510772baff5af00653ed6f382abf1298d72b
cody <codyberlin@gmail.com>
2016-11-15 18:55:01 -0800

    added two projects and a contact form, started adding velocity animations and functionality:

--- js/control.js
+++ js/control.js
@@ -1,3 +1,11 @@
 $('#projects-button').on('click', function() {
-  $('#phaser').fadeOut(1000);
+  $('main').velocity('scroll', {duration:1500}).velocity({opacity:1});
+});
+
+$('#contact-me-button').on('click', function() {
+  $('footer').velocity('scroll', {duration:1500}).velocity({opacity:1});
+});
+
+$('#home-star').on('click', function() {
+  $('body').velocity('scroll', {duration:1500}).velocity({opacity:1});
 });
