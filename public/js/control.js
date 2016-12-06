f628e2913b9249c562644f2cabf61db5acaf68e9
cody <codyberlin@gmail.com>
2016-12-06 14:11:47 -0800

    ORevert "starting working on some more velocity animations"
    This reverts commit 414093224321ffe11ce64af79913ed877331e647.

--- public/js/control.js
+++ public/js/control.js
@@ -1,25 +1,16 @@
-var controller = new ScrollMagic.Controller();
-
-var projectScene = new ScrollMagic.Scene({triggerElement: "#project-trigger"})
-					// trigger a velocity opaticy animation
-					.setVelocity(".card", "transition.slideLeftIn", { stagger: 400 })
-					.addTo(controller);
-
-var ContactScene = new ScrollMagic.Scene({triggerElement: '#contact-trigger'})
-					.setVelocity(".row", "transition.slideLeftIn", { stagger: 400 })
-					.addTo(controller);
-
 $('#projects-button').on('click', function() {
-  $('#card-container').velocity('scroll', {duration:1000}).velocity({opacity:1});
+  $('main').velocity('scroll', {duration:1500}).velocity({opacity:1});
   destroyGame();
 });
 
 $('#contact-me-button').on('click', function() {
-  $('footer').velocity('scroll', {duration:1000}).velocity({opacity:1});
+  $('footer').velocity('scroll', {duration:1500}).velocity({opacity:1});
   destroyGame();
 });
 
-
+$('#home-star').on('click', function() {
+  $('body').velocity('scroll', {duration:1500}).velocity({opacity:1});
+});
 
 function destroyGame() {
 	game.destroy();
