6b3b9246904396915469ba437b964ea03bcf79b4
cody <codyberlin@gmail.com>
2016-12-10 13:19:55 -0800

    fixed some animation timing, testing for full responsiveness on mobile. Navbar now working on mobile

--- public/js/control.js
+++ public/js/control.js
@@ -4,15 +4,15 @@ console.log(width);
 
 var controller = new ScrollMagic.Controller();
 
-var tween = TweenMax.staggerFromTo(['#project-head', '#text-bug', '#recipe-calc', '#asteroids', '#news-ping'], 5,
-    {autoAlpha:0,ease:Power4.easeInOut,x:-100}, 
+var tween = TweenMax.staggerFromTo(['#project-head', '#text-bug', '#recipe-calc', '#asteroids', '#news-ping'], 150,
+    {autoAlpha:0,ease:Power4.easeInOut,x:+100}, 
     {autoAlpha:1,ease:Power4.easeInOut,x:0}
- , 1);
+ , 70);
 
 if (width < 768) {
 	var scene = new ScrollMagic.Scene({
     triggerElement: '#project-container',
-    duration: 1000 
+    duration: 1300 
   })
   .setTween(tween)
   .addTo(controller);
@@ -27,12 +27,11 @@ else {
 	}
   
 
-scene.addIndicators();
 
-var showAboutMe = TweenMax.staggerFromTo(["#about-me-header", "#about-me-card"], 5,
+var showAboutMe = TweenMax.staggerFromTo(["#about-me-header", "#about-me-card"], 50,
 	{autoAlpha:0,ease:Power4.easeInOut,x:-100}, 
     {autoAlpha:1,ease:Power4.easeInOut, x:0}, 
-    4);
+    10);
 
 var scene = new ScrollMagic.Scene({
     triggerElement: '#about-me-container',
@@ -42,44 +41,44 @@ var scene = new ScrollMagic.Scene({
   .addTo(controller);
   
  
-scene.addIndicators();
 
 
 
 
-var showContactMe = TweenMax.staggerFromTo(["#contact-header", ".row"], 5,
-    {autoAlpha:0,ease:Power4.easeInOut, y:+100}, 
-    {autoAlpha:1,ease:Power4.easeInOut, y:0}
- , 1.5);
+
+var showContactMe = TweenMax.staggerFromTo(["#contact-header", ".row"], 50,
+    {autoAlpha:0,ease:Power4.easeInOut, x:-100}, 
+    {autoAlpha:1,ease:Power4.easeInOut, x:0}
+ , 10);
 
 var scene = new ScrollMagic.Scene({
-    triggerElement: '#contact-container',
+    triggerElement: '#show-contact',
     duration: 200 
   })
   .setTween(showContactMe)
   .addTo(controller);
   
  
-scene.addIndicators();
 
 
 
 
-var moveWords = TweenMax.staggerFromTo(["#node", "#html", "#css3", "#redux", "#react", "#git", "#mongodb", "#express"], 5,
-    {autoAlpha:0,ease:Power4.easeInOut, y:+500}, 
-    {autoAlpha:1,ease:Power4.easeInOut, y:-100}
- ,3);
+
+var moveWords = TweenMax.staggerFromTo(["#node", "#html", "#css3", "#redux", "#react", "#git", "#mongodb", "#express"], 500,
+    {autoAlpha:0,ease:Power4.easeInOut, x:+500}, 
+    {autoAlpha:1,ease:Power4.easeInOut, x:0}
+ ,30);
 
 var scene = new ScrollMagic.Scene({
     triggerElement: '#play-stagger-words',
-    duration: 400 
+    duration: 600 
   })
   
   .setTween(moveWords)
   .addTo(controller);
   
  
-scene.addIndicators();
+
 
 
 
